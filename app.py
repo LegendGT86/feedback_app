6005ff6100e7245ac74ec3e749b41be98c9b0d7f
Dylan Matheson <dmatheson@bonline.com>
2024-02-18 10:33:48 +0200

    base created

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin/Feedback'
+
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__='feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self,customer,dealer,rating,comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-if __name__== '__main__':
-    app.debug= True
-    app.run()
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -17,11 +43,10 @@ def submit():
         dealer = request.form['dealer']
         rating = request.form['rating']
         comments = request.form['comments']
-        #print(customer,dealer,rating,comments)
+
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please complete all required fields')
     return render_template('success.html')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
